67dba8ae3536cdc3c0e3b65dc453cf5ba321ec16
nguoinaodo <luugu196@gmail.com>
2017-10-25 13:53:46 +0700

    Fix

--- author_paper.php
+++ author_paper.php
@@ -4,7 +4,7 @@
 	include 'import/import_author_paper.php';
 	include 'db.php';
 	// Import author-paper
-	$start = intval($argv[1])
-	$limit = intval($argv[2])
+	$start = intval($argv[1]);
+	$limit = intval($argv[2]);
 	import_author_paper($raw_db, $start, $limit, $main_db);
 ?>
